da36a16e00e2597bef05e4ae01bc244475f972f9
Adisak Rungruang <adisak01234@gmail.com>
2024-05-17 09:07:32 +0700

    make it containerized

--- tests/test_flask_app.py
+++ tests/test_flask_app.py
@@ -1,10 +1,9 @@
-from app import create_app
+from app.main import app
 import pytest
 
 
 @pytest.fixture
 def client():
-    app = create_app()
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
